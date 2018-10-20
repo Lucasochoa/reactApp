bcf353e8cd940dbfa4ff967935e5f96b371f0023
Lucasochoa <lucasochoa96@gmail.com>
2018-10-19 22:25:06 -0400

    initial commit

---
+++ src/components/TitleWord.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../App.css';
+import './TitleWord.css';
+
+
+class TitleWord extends Component{
+
+  render() {
+    return (
+      <div className = "spaceBox">
+        <p className="titleWord">PLACES THAT ARE GOOD</p>
+      </div>
+    );
+  }
+}
+
+
+export default TitleWord;
